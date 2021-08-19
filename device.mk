43e849d13b6c07268654109f8d84c90eb63a55c7
yigityanik <yeyanik123@gmail.com>
2021-08-19 21:06:03 +0300

    GM8_sprout: uprev audio, fix sound issue

--- device.mk
+++ device.mk
@@ -190,11 +190,14 @@ PRODUCT_PACKAGES_DEBUG += \
 	
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service \
+    audio.primary.default \
     audio.primary.msm8937 \
     audio.r_submix.default \
     audio.usb.default \
